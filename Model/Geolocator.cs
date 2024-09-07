a7d7eaf2833e741a42b2252c8f9571a8e5e575a3
expirius <vladworldof@gmail.com>
2024-09-07 13:19:37 +0300

    Убрал спейсы, избавился от старого ненужного класса компаса

--- Model/Geolocator.cs
+++ Model/Geolocator.cs
@@ -1,16 +1,51 @@
-﻿namespace MFASeeker;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Mapsui.Extensions;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
-public class Geolocator
+namespace MFASeeker;
+
+public partial class Geolocator : ObservableObject
 {
     public static IGeolocator Default = new GeolocatorImplementation();
     CancellationTokenSource _cancelTokenSource = new CancellationTokenSource();
-    Location Location = new();
 
-    public async Task StartUpdateLocationAsync()
+    public Geolocator(double reading = 100, double rotationAngle = 0)
     {
-        try 
-        { 
-            
+        Reading = reading ; 
+        RotationAngle = rotationAngle;
+    }
+
+    [ObservableProperty]
+    private double reading;
+    [ObservableProperty]
+    private double rotationAngle;
+
+    public static Action<Geolocator>? OnCompassChangedAction { get; set; }
+
+    private void OnCompassReadingChanged(object sender, CompassChangedEventArgs e)
+    {
+        Reading = e.Reading.HeadingMagneticNorth;
+        RotationAngle = 360 - e.Reading.HeadingMagneticNorth;
+    }
+
+    public async Task StartUpdateCompassAsync()
+    {
+        try
+        {
+            if (Compass.Default.IsSupported)
+            {
+                if (!Compass.Default.IsMonitoring)
+                {
+                    Compass.Default.ReadingChanged += OnCompassReadingChanged;
+                    Compass.Default.Start(SensorSpeed.UI);
+                }
+                else
+                {
+                    Compass.Default.Stop();
+                    Compass.Default.ReadingChanged -= OnCompassReadingChanged;
+                }
+            }
         }
         catch (TaskCanceledException ex) { Console.WriteLine(ex.ToString()); }
     }
