530b696789c8edaab2ebe58a0b994b0e5e0bec0f
expirius <vladworldof@gmail.com>
2024-09-21 23:51:34 +0300

    Создан starselector и небольшая логика с bindable (нужны доработки)

---
+++ Converters/StarConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFASeeker.Converters
+{
+    /*
+     * Если ИНДЕКС меньше или равен РЕЙТИНГУ, звезда ЗОЛОТАЯ
+     * Если ИНДЕКС больше РЕЙТИНГА, звезда БЕЛАЯ
+     */
+    public class StarConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            int rating = (int)value;
+            int starIndex = int.Parse(parameter.ToString());
+            // пикчи золото или белый
+            return starIndex <= rating ? "gold_startoilet.png" : "white_startoilet.png";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
