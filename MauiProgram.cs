45f1880ac702dc342f9d8c7bcec200d1ac999f0d
expirius <vladworldof@gmail.com>
2024-09-24 21:33:45 +0300

    Проделана логика работы попапа, связаны с попапо

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using MFASeeker.View;
+﻿using CommunityToolkit.Maui;
+using MFASeeker.View;
 using MFASeeker.ViewModel;
 using Microsoft.Extensions.Logging;
 using SkiaSharp.Views.Maui.Controls.Hosting;
@@ -12,6 +13,7 @@ namespace MFASeeker
         {
             var builder = MauiApp.CreateBuilder();
 
+            // MAPSUI
             builder
                 .UseMauiApp<App>()
                 .UseSkiaSharp(true) // Карты MAPSUI
@@ -20,6 +22,10 @@ namespace MFASeeker
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            // Community Toolkit MAUI от mc с кастомными элементами
+            builder.UseMauiApp<App>()
+                .UseMauiCommunityToolkit();
+            //
             builder.Services.AddSingleton<MainPage>();
             builder.Services.AddSingleton<MainViewModel>();
             builder.Services.AddSingleton<SearchViewModel>();
