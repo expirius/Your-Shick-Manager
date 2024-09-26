f90a27d6159bd184dec429f2e6c2bcc94f3e5b08
expirius <vladworldof@gmail.com>
2024-09-26 17:39:22 +0300

    Отказ от немнужного класса

--- Model/Toilet.cs
+++ Model/Toilet.cs
@@ -2,8 +2,7 @@
 
 namespace MFASeeker.Model
 {
-    [ObservableObject]
-    public partial class Toilet
+    public partial class Toilet : ObservableObject
     {
         [ObservableProperty]
         private int id;
@@ -16,6 +15,14 @@ namespace MFASeeker.Model
         [ObservableProperty]
         private double rating;
 
+        public string? UserName { get; set; }
+        public string? UserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public string GetMarkerInfo()
+        {
+            return $"Name: {Name}, Location: {Location}, Description: {Description}, Rating: {Rating}, Created By: {UserId}, On: {CreatedDate}";
+        }
         public bool IsValid()
         {
             return !string.IsNullOrWhiteSpace(Name);
