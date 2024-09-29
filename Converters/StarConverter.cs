0bd72db1e30c70abd9301c64abcdb91dc26205c0
expirius <vladworldof@gmail.com>
2024-09-29 21:52:56 +0300

    Видоизменен ненужный конвертер для его использования. Теперь Он возвращает путь к пнг для ранга

--- Converters/StarConverter.cs
+++ Converters/StarConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MFASeeker.Model;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -7,18 +8,15 @@ using System.Threading.Tasks;
 
 namespace MFASeeker.Converters
 {
-    /*
-     * Если ИНДЕКС меньше или равен РЕЙТИНГУ, звезда ЗОЛОТАЯ
-     * Если ИНДЕКС больше РЕЙТИНГА, звезда БЕЛАЯ
-     */
-    public class StarConverter : IValueConverter
+    public class RaitingPNGConverter : IValueConverter
     {
         public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            int rating = (int)value;
-            int starIndex = int.Parse(parameter.ToString());
-            // пикчи золото или белый
-            return starIndex <= rating ? "gold_startoilet.png" : "white_startoilet.png";
+            if (value is (double tmp))
+            {
+                return ToiletIconProvider.GetIconName(tmp);
+            }
+            return @"defaultrank_toilet.svg";
         }
 
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
