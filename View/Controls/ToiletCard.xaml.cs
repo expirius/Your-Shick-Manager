54a02b98510fe93495cea99c54f81026f541b677
expirius <vladworldof@gmail.com>
2024-10-03 00:24:25 +0300

    Обновлена карточка на странице управления точками. Теперь отображается коллекция Toilet Card от ActiveList

--- View/Controls/ToiletCard.xaml.cs
+++ View/Controls/ToiletCard.xaml.cs
@@ -1,3 +1,5 @@
+using MFASeeker.Model;
+
 namespace MFASeeker.View.Controls;
 
 public partial class ToiletCard : ContentView
@@ -6,4 +8,23 @@ public partial class ToiletCard : ContentView
 	{
 		InitializeComponent();
 	}
+	public Toilet Toilet
+	{
+		get => (Toilet)GetValue(ToiletProperty);
+		set => SetValue(ToiletProperty, value);
+	}
+	public static readonly BindableProperty ToiletProperty =
+		BindableProperty.Create(
+			nameof(ToiletProperty),
+			typeof(Toilet),
+			typeof(ToiletCard),
+			null,
+			propertyChanged: OnToiletChanged);
+	public static void OnToiletChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+        if (bindable is ToiletCard card)
+        {
+            card.SetValue(ToiletProperty, newValue);
+        }
+    }
 }
