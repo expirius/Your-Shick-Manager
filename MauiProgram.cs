e362233095ebd78c066d8c615f7335a83f7ced94
expirius <vladworldof@gmail.com>
2024-10-09 23:26:45 +0300

    fix }

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -2,41 +2,43 @@
 using MFASeeker.View;
 using MFASeeker.ViewModel;
 using Microsoft.Extensions.Logging;
-using Microsoft.Maui.Handlers;
-using Microsoft.Maui.Platform;
 using SkiaSharp.Views.Maui.Controls.Hosting;
 //using Material.Components.Maui.Extensions;
 
-namespace MFASeeker;
-
-public static class MauiProgram
+namespace MFASeeker
 {
-    public static MauiApp CreateMauiApp()
+    public static class MauiProgram
     {
-        var builder = MauiApp.CreateBuilder();
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
 
-        // MAPSUI
-        builder
-            .UseMauiApp<App>()
-            .UseSkiaSharp(true) // Карты MAPSUI
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
-        // Community Toolkit MAUI от mc с кастомными элементами
-        builder.UseMauiApp<App>()
-            .UseMauiCommunityToolkit();
-        //
-        builder.Services.AddSingleton<MainPage>();
-        builder.Services.AddSingleton<MainViewModel>();
+            // MAPSUI
+            builder
+                .UseMauiApp<App>()
+                .UseSkiaSharp(true) // Карты MAPSUI
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+            // Community Toolkit MAUI от mc с кастомными элементами
+            builder.UseMauiApp<App>()
+                .UseMauiCommunityToolkit();
+            //
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
 
-        builder.Services.AddSingleton<SearchViewModel>();
-        builder.Services.AddSingleton<SearchPage>();
+            builder.Services.AddSingleton<SearchViewModel>();
+            builder.Services.AddSingleton<SearchPage>();
 
-        builder.Services.AddSingleton<PinManagerPage>();
-        builder.Services.AddSingleton<PinManagerViewModel>();
-        #if DEBUG
-        builder.Logging.AddDebug();
-        #endif
+            builder.Services.AddSingleton<PinManagerPage>();
+            builder.Services.AddSingleton<PinManagerViewModel>();
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
 }
