30945f6a57c0d240079865d001633f5f20607ae6
expirius <vladworldof@gmail.com>
2024-10-24 14:23:38 +0300

    Рефакторинг проекта. Теперь в нём находятся все проекты. Сервер, модели, клиентское приложение

---
+++ Entities/Toilet.cs
@@ -0,0 +1,36 @@
+﻿namespace Entities
+{
+    public class Toilet
+    {
+        public int Id { get; set; }
+        public string Guid { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public Location? Location { get; set; }
+        public string? Description { get; set; } = string.Empty;
+        public double Rating { get; set; } = 0;
+        public string? UserName { get; set; }
+        public string? UserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<ImageFile> Images { get; set; } = [];
+
+        public Toilet()
+        {
+            this.Guid = CreateGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+        public string GetInfo()
+        {
+            return $"Name: {Name}, Location: {Location}, Description: {Description}, Rating: {Rating}, Created By: {UserId}, On: {CreatedDate}";
+        }
+        public bool IsValid()
+        {
+            return !string.IsNullOrWhiteSpace(Name);
+        }
+        private string CreateGuid()
+        {
+            return System.Guid.NewGuid().ToString().GetHashCode().ToString("x");
+        }
+
+    }
+}
