1840ab4c8397d5d1a38fbebc42c55c31c67ba9e1
expirius <vladworldof@gmail.com>
2024-10-26 00:41:26 +0300

    Почти полный отказ от entities.

---
+++ Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class User : BaseEntity
+    {
+        public virtual string UserName { get; set; } = string.Empty;
+        public virtual string DeviceInfo { get; set; } = string.Empty;
+        //public virtual string Password {  get; set; } = string.Empty;
+    }
+}
