7bece72f11339dc09bdf1d5849ebff4a4694738a
expirius <vladworldof@gmail.com>
2024-10-26 23:56:42 +0300

    Основа контроллеров

--- MFASeekerServer/Controllers/ToiletController.cs
+++ MFASeekerServer/Controllers/ToiletController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MFASeekerServer.Core.Entities;
+using MFASeekerServer.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MFASeekerServer.Controllers
 {
@@ -7,5 +10,30 @@ namespace MFASeekerServer.Controllers
     public class ToiletController : ControllerBase
     {
         //private readonly IToiletService _toiletService;
+        private readonly SeekerDbContext _context;
+        ToiletController(SeekerDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        [HttpGet("AllToilets")]
+        public async Task<ActionResult<List<Toilet>>> GetToilets()
+        {
+            return Ok(await _context.Toilets.ToListAsync());
+        }
+        // В будущем сделать через слои приложения
+        //public async Task<IActionResult> AddToilet([FromBody] ToiletDto toiletDto)
+        //var createdToilet = await _toiletService.AddToiletAsync(toiletDto);
+
+        // Временное решение
+        [HttpPost]
+        public async Task<ActionResult<Toilet>> AddToilet(Toilet createdToilet)
+        {
+            _context.Toilets.Add(createdToilet);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Toilets.ToListAsync());
+        }
+
     }
 }
---
+++ MFASeekerServer/Controllers/UserContoller.cs
@@ -0,0 +1,40 @@
+﻿using MFASeekerServer.Core.Entities;
+using MFASeekerServer.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MFASeekerServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserContoller : ControllerBase
+    {
+        private readonly SeekerDbContext _context;
+        public UserContoller(SeekerDbContext context)
+        {
+            _context = context;
+        }
+        // GET: AllUsers
+        [HttpGet("AllUsers")]
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+        // POST: UserContoller/Create
+        [HttpPost("AddUser")]
+        public ActionResult Create(User newUser)
+        {
+            try
+            {
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+                return Ok($"User was added: {newUser.Id}, {newUser.UserName}");
+            }
+            catch
+            {
+                return BadRequest("Bad try :(");
+            }
+        }
+    }
+}
