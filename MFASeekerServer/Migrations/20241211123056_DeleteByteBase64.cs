56997cfc63ac40c7016a48e722ec3c898269339c
expirius <vladworldof@gmail.com>
2024-12-11 16:26:00 +0300

    migrations about bytebase can null

---
+++ MFASeekerServer/Migrations/20241211123056_DeleteByteBase64.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MFASeekerServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteByteBase64 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToiletImages_ImageFiles_ImageID",
+                table: "ToiletImages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ByteBase64",
+                table: "ImageFiles",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToiletImages_ImageFiles_ImageID",
+                table: "ToiletImages",
+                column: "ImageID",
+                principalTable: "ImageFiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToiletImages_ImageFiles_ImageID",
+                table: "ToiletImages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ByteBase64",
+                table: "ImageFiles",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToiletImages_ImageFiles_ImageID",
+                table: "ToiletImages",
+                column: "ImageID",
+                principalTable: "ImageFiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
