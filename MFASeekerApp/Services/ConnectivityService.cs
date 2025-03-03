91c321ea383b77d3c5ace8424a9dbf921a208b64
expirius <vladworldof@gmail.com>
2025-03-03 18:13:36 +0300

    Добавлен сервис для отслеживания подключения к интернету

---
+++ MFASeekerApp/Services/ConnectivityService.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFASeekerApp.Services
+{
+    public class ConnectivityService
+    {
+        public ConnectivityService()
+        {
+            Connectivity.ConnectivityChanged += OnConnectivityChanged;
+            CheckInternetStatus();
+        }
+
+        private void OnConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            CheckInternetStatus();
+        }
+
+        private void CheckInternetStatus()
+        {
+            bool isConnected = Connectivity.NetworkAccess == NetworkAccess.Internet;
+
+            // Отправляем сообщение во всё приложение
+            WeakReferenceMessenger.Default.Send(new ConnectivityMessage(isConnected));
+        }
+    }
+
+    // Сообщение для уведомлений о сети
+    public record ConnectivityMessage(bool IsConnected);
+}
